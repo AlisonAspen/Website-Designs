7616b2e597b98c956f3d7db062735b3b9d1741ea
alison-lake5media <96205238+alison-lake5media@users.noreply.github.com>
2022-04-18 11:18:16 -0400

    v1 changes

--- styles_v1/assets/main.js
+++ styles_v1/assets/main.js
@@ -1,15 +1,1 @@
-let picArray = ["styles_v1/assets/handshake_stock.jpg", "styles_v1/assets/laptop_graph_stock.jpg", "styles_v1/assets/paper_graph_stock.jpg"];
-
-function loadPicture(pic) {
-    document.getElementById("img-holder").style = "background-image: url('" + picArray[pic] + "');";
-    
-
-    for(i = 0; i < picArray.length; i++) {
-        if(i = pic) {
-            document.getElementsByClassName("dot")[pic].style = "background-color: red;";
-            return 0;
-        }
-        
-    }
-}
 
